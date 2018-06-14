439d3abc090f05eecdf74595bc3cdb8d536bbf1e
Coleman Imhoff <colemanimhoff@gmail.com>
2018-06-13 19:37:01 -0600

    added navbar and header to ProblemList

--- src/components/ProblemList.jsx
+++ src/components/ProblemList.jsx
@@ -1,11 +1,63 @@
 import React from "react";
-// import Problem from "./Problem";
+import Problem from "./Problem";
+import NavBar from "./Navbar"
+import { Header } from 'semantic-ui-react';
 
+const apiUrl = "https://galvanize-queue-overflow.herokuapp.com/";
 
-const ProblemList = ({ problems }) => {
-  return (
-    <div>Hello</div>
-  );
-};
+class ProblemList extends React.Component {
+
+  state = {
+    problems: [],
+    tags: []
+  }
+
+  componentDidMount() {
+    fetch(apiUrl + 'problems')
+      .then(response => response.json())
+      .then(problems => {
+        return this.setState({ problems: problems });
+      }).then(() => {
+        fetch(`http://galvanize-queue-overflow.herokuapp.com/problem/tags`)
+          .then(res => res.json())
+          .then(tagProblems => {
+            fetch(`http://galvanize-queue-overflow.herokuapp.com/tags`)
+              .then(res => res.json())
+              .then(tags => {
+                let currentProblemsTags = tagProblems.reduce((acc, curr, i) => {
+                  if (curr["problem_id"] == this.state.problem["id"]) {
+                    let currTagInfo = {
+                      tag_id: curr.tag_id,
+                      problem_id: curr.problem_id,
+                      tag_name: tags[curr.tag_id - 1]["tag_name"]
+                    };
+                    acc.push(currTagInfo);
+                  }
+                  return acc;
+                }, []);
+                this.setState({ tags: currentProblemsTags });
+              });
+          })
+      })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Header as="h2">Open Issues</Header>
+        <div>
+          {this.state.problems
+            ?
+            this.state.problems.map((problem, i) => {
+              return <Problem key={i} problem={problem} />
+            })
+            : ''}
+        </div>
+      </React.Fragment >
+
+    )
+  }
+}
 
 export default ProblemList
