e82b283f64bd96b119a6e168d0b63346beb591ab
Coleman Imhoff <colemanimhoff@gmail.com>
2018-06-14 21:45:37 -0600

    merging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,8 +16,8 @@ class Navbar extends Component {
     searchTerm: '',
     redirect: false
   };
-  handleItemClick = (e, {name}) => {
-    this.setState({activeItem: name});
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
   }
   handleChange = (event) => {
     event.preventDefault();
@@ -31,36 +31,22 @@ class Navbar extends Component {
   }
   handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.setState({redirect: true});
+      this.setState({ redirect: true });
     }
 
   }
   render() {
     const { activeItem } = this.state;
     if (this.state.redirect) {
-<<<<<<< HEAD
       return (<Redirect to={{
         pathname: '/queries/' + encodeURIComponent(this.state.searchTerm),
         state: { searchTerm: this.state.searchTerm }
       }} />)
-=======
-      return (<Redirect
-        to={{
-        pathname: '/queries/' + encodeURIComponent(this.state.searchTerm),
-        state: {
-          searchTerm: this.state.searchTerm
-        }
-      }}/>)
->>>>>>> 6db3f2018dd01e27e55cf46fb2b258dfe173dada
     }
     return (
       <Segment className="col-12 navbar" inverted id="navbar-container">
         <Menu attached inverted pointing secondary>
-<<<<<<< HEAD
           <img src={GLogo} alt="Galvanize Logo" className="galv-logo" />
-=======
-          <img src={GLogo} alt="Galvanize Logo" className="galv-logo"/>
->>>>>>> 6db3f2018dd01e27e55cf46fb2b258dfe173dada
           <Menu.Item
             as={NavLink}
             to="/"
@@ -76,9 +62,6 @@ class Navbar extends Component {
             active={activeItem === "questions"}
             onClick={this.handleItemClick} />
           <Menu.Item position="right">
-<<<<<<< HEAD
-            <Input onChange={this.handleChange} onKeyPress={this.handleKeyPress} name="searchTerm" className="icon" value={this.state.searchTerm} icon="search" placeholder="Search..." id="searchbar" />
-=======
             <Input
               onChange={this.handleChange}
               onKeyPress={this.handleKeyPress}
@@ -87,8 +70,7 @@ class Navbar extends Component {
               value={this.state.searchTerm}
               icon="search"
               placeholder="Search..."
-              id="searchbar"/>
->>>>>>> 6db3f2018dd01e27e55cf46fb2b258dfe173dada
+              id="searchbar" />
           </Menu.Item>
           {/* <Menu.Item
             name="ask"
@@ -98,7 +80,6 @@ class Navbar extends Component {
             name="users"
             active={activeItem === "users"}
             onClick={this.handleItemClick}/> */}
-<<<<<<< HEAD
           {
             this.props.token ?
               (
@@ -119,22 +100,6 @@ class Navbar extends Component {
               )
           }
           <img src={GithubLogo} alt="github logo" />
-=======
-          {this.props.token
-            ? (<Menu.Item
-              name="Log Out"
-              active={activeItem === "users"}
-              onClick={this.props.logOutUser}
-              position="right"/>)
-            : (<Menu.Item
-              href="https://github.com/login/oauth/authorize?client_id=e75e8c72d121f49f6a1b"
-              name="Sign In"
-              active={activeItem === "users"}
-              onClick={this.handleItemClick}
-              position="right"/>)
-}
-          <img src={GithubLogo} alt="github logo"/>
->>>>>>> 6db3f2018dd01e27e55cf46fb2b258dfe173dada
         </Menu>
       </Segment>
     )
