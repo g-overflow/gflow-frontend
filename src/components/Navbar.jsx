618045f0771cf21c025883a1a780ffa4707ef191
ebectar <acebe424@gmail.com>
2018-06-14 21:41:24 -0600

    minor styling changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Segment, Menu, Input} from "semantic-ui-react";
-import { NavLink, Redirect } from "react-router-dom";
+import {NavLink, Redirect} from "react-router-dom";
 import GLogo from "../assets/g-logo-small.png"
 import GithubLogo from "../assets/github-logo.png"
 
@@ -10,31 +10,36 @@ class Navbar extends Component {
     searchTerm: '',
     redirect: false
   };
-  handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name });
+  handleItemClick = (e, {name}) => {
+    this.setState({activeItem: name});
   }
   handleChange = (event) => {
     event.preventDefault();
-    this.setState({[event.target.name]: [event.target.value]})
+    this.setState({
+      [event.target.name]: [event.target.value]
+    })
   }
   handleKeyPress = (e) => {
     if (e.key === 'Enter') {
-      this.setState({ redirect: true });
+      this.setState({redirect: true});
     }
 
   }
   render() {
     const {activeItem} = this.state;
     if (this.state.redirect) {
-      return (<Redirect to={{
-                pathname: '/queries/' + encodeURIComponent(this.state.searchTerm),
-                state: { searchTerm: this.state.searchTerm }
-            }} />)
+      return (<Redirect
+        to={{
+        pathname: '/queries/' + encodeURIComponent(this.state.searchTerm),
+        state: {
+          searchTerm: this.state.searchTerm
+        }
+      }}/>)
     }
     return (
       <Segment className="col-12 navbar" inverted id="navbar-container">
         <Menu attached inverted pointing secondary>
-        <img src={GLogo} alt="Galvanize Logo" className="galv-logo"/>
+          <img src={GLogo} alt="Galvanize Logo" className="galv-logo"/>
           <Menu.Item
             as={NavLink}
             to="/"
@@ -50,7 +55,15 @@ class Navbar extends Component {
             active={activeItem === "questions"}
             onClick={this.handleItemClick}/>
           <Menu.Item position="right">
-            <Input onChange={this.handleChange} onKeyPress={this.handleKeyPress} name="searchTerm" className="icon" value={this.state.searchTerm} icon="search" placeholder="Search..." id="searchbar"/>
+            <Input
+              onChange={this.handleChange}
+              onKeyPress={this.handleKeyPress}
+              name="searchTerm"
+              className="icon"
+              value={this.state.searchTerm}
+              icon="search"
+              placeholder="Search..."
+              id="searchbar"/>
           </Menu.Item>
           {/* <Menu.Item
             name="ask"
@@ -60,26 +73,20 @@ class Navbar extends Component {
             name="users"
             active={activeItem === "users"}
             onClick={this.handleItemClick}/> */}
-          {
-            this.props.token ?
-              (
-                <Menu.Item
-                  name="Log Out"
-                  active={activeItem === "users"}
-                  onClick={this.props.logOutUser}
-                position="right"/>
-              )
-              :
-              (
-                <Menu.Item
-                  href="https://github.com/login/oauth/authorize?client_id=e75e8c72d121f49f6a1b"
-                  name="Sign In"
-                  active={activeItem === "users"}
-                  onClick={this.handleItemClick}
-                  position="right" />
-              )
-          }
-          <img src={GithubLogo} alt="github logo" />
+          {this.props.token
+            ? (<Menu.Item
+              name="Log Out"
+              active={activeItem === "users"}
+              onClick={this.props.logOutUser}
+              position="right"/>)
+            : (<Menu.Item
+              href="https://github.com/login/oauth/authorize?client_id=e75e8c72d121f49f6a1b"
+              name="Sign In"
+              active={activeItem === "users"}
+              onClick={this.handleItemClick}
+              position="right"/>)
+}
+          <img src={GithubLogo} alt="github logo"/>
         </Menu>
       </Segment>
     )
